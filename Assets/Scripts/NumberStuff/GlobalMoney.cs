9888dfdf37067044c442b90f0485b9aeab087ce3
82gnawf <82gnawf@users.noreply.github.com>
2019-07-22 00:01:54 -0700

    blue and red slime stuff

--- Assets/Scripts/NumberStuff/GlobalMoney.cs
+++ Assets/Scripts/NumberStuff/GlobalMoney.cs
@@ -10,14 +10,31 @@ public class GlobalMoney : MonoBehaviour
     public static int MoneyCount;
     public int InternalMoneyCount;
 
+    public GameObject greenPrice;
+    public GameObject bluePrice;
+    public GameObject redPrice;
+
     public static int greenWorth = 2;
     public static int blueWorth = 4;
     public static int redWorth = 5;
+
+    public int intGreenWorth;
+    public int intBlueWorth;
+    public int intRedWorth;
 
     public static int MoneyForPrestige;
 
     private void Update() {
         InternalMoneyCount = MoneyCount;
         MoneyDisplay.GetComponent<Text>().text = "Money: $" + InternalMoneyCount; //display
+
+        intGreenWorth = greenWorth;
+        greenPrice.GetComponent<Text>().text = "Value: $" + intGreenWorth;
+
+        intBlueWorth = blueWorth;
+        bluePrice.GetComponent<Text>().text = "Value: $" + intBlueWorth;
+
+        intRedWorth = redWorth;
+        redPrice.GetComponent<Text>().text = "Value: $" + intRedWorth;
     }
 }
--- Assets/Scripts/NumberStuff/GlobalSlime.cs
+++ Assets/Scripts/NumberStuff/GlobalSlime.cs
@@ -5,13 +5,34 @@ using UnityEngine.UI;
 
 public class GlobalSlime : MonoBehaviour
 {
-    public GameObject SlimeDisplay;
+    //green 
+    public GameObject GreenSlimeDisplay;
 
     public static int greenSlimeCount;
-    public int InternalgreenSlimeCount;
+    public int internalGreenSlimeCount;
+
+    //blue 
+    public GameObject BlueSlimeDisplay;
+
+    public static int blueSlimeCount;
+    public int internalBlueSlimeCount;
+
+    //red 
+    public GameObject RedSlimeDisplay;
+
+    public static int redSlimeCount;
+    public int internalRedSlimeCount;
 
     private void Update() {
-        InternalgreenSlimeCount = greenSlimeCount; 
-        SlimeDisplay.GetComponent<Text>().text = "X " + InternalgreenSlimeCount; //display
+        internalGreenSlimeCount = greenSlimeCount;
+        GreenSlimeDisplay.GetComponent<Text>().text = "X " + internalGreenSlimeCount;
+        internalBlueSlimeCount = blueSlimeCount;
+        BlueSlimeDisplay.GetComponent<Text>().text = "X " + internalBlueSlimeCount;
+        internalRedSlimeCount = redSlimeCount;
+        RedSlimeDisplay.GetComponent<Text>().text = "X " + internalRedSlimeCount;
     }
+
+
+
+
 }
