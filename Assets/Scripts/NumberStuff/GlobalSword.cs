691f57698b8ce43ad2cb375a2dd475817e31f47b
82gnawf <82gnawf@users.noreply.github.com>
2019-08-10 09:12:37 -0700

    Panel work and fixing

---
+++ Assets/Scripts/NumberStuff/GlobalSword.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalSword : MonoBehaviour
+{
+
+    public static int SwordDamage;
+    public static int SwordRate;
+    public static int SwordCrit;
+    public static int SwordCritRate;
+
+}
