5fba433bad1049c70f8506e5b9552f8773f3b22b
82gnawf <82gnawf@users.noreply.github.com>
2019-08-15 13:40:18 -0700

    A bit of work done to Sword Stats
    try and do some more anim stuff first for QOL debugging

--- Assets/Scripts/Buttons/DamageButton.cs
+++ Assets/Scripts/Buttons/DamageButton.cs
@@ -8,10 +8,36 @@ public class DamageButton : MonoBehaviour {
 
     public GameObject SwordSwing;
 
+    public int CritRand;
+    public int HitRand;
+
+    public double CritRate;
+    public double HitRate;
+
 	public void ButtonClick () {
-     
-        GlobalHp.TempHp -= GlobalSword.SwordDamage;
+
+        CritRate = GlobalSword.SwordCritRate;
+        HitRate = GlobalSword.SwordRate;
+        System.Random randomCR = new System.Random();
+        System.Random randomHR = new System.Random();
+        CritRand = randomCR.Next(1, 101); //between 1 and 100 
+        HitRand = randomHR.Next(1, 101); 
         SwordSwing.GetComponent<Animation>().Play("SwordAnim");
+
+        if (HitRand <= HitRate) {
+            if (CritRand <= CritRate) {
+                GlobalHp.TempHp -= GlobalSword.SwordCrit;
+            }
+            else {
+                GlobalHp.TempHp -= GlobalSword.SwordDamage;
+            }
+        }
+        else{
+            //missedAnim;
+        }
+
+
+        GlobalHp.TempHp -= GlobalSword.SwordDamage;
 
     }
 }
